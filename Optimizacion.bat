0cc866782054921f7bc221c0989c4883a0067bbc
JuanDa-21 <juan.garcia-gar@uniminuto.edu.co>
2025-09-03 18:13:44 -0500

    Add files via upload

---
+++ Optimizacion.bat
@@ -0,0 +1,30 @@
+@echo off
+cd ..
+cd documents
+mkdir Optimizacion
+cd Optimización 
+mkdir Codigo
+cd Codigo
+Python--version 
+py -m venv .venv
+call ,venv/Scripts/actívate.bat
+pip install jupyter
+mkdir src
+mkdir notebook
+cd src
+mkdir models
+cd models 
+type nul > models.py
+cd ..
+mkdir view
+cd view
+type nul > view.py
+cd ..
+mkdir controller
+type nul > controller.py
+cd ..
+cd ..
+cd notebook
+type nul > Prueba.ipynb
+cd ..
+code .
