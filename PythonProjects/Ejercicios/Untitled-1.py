5ce582c2e3350d6e279d024006386abe707006d2
JuanDa-21 <juan.garcia-gar@uniminuto.edu.co>
2025-09-17 15:32:46 -0500

    Update Untitled-1.py

--- PythonProjects/Ejercicios/Untitled-1.py
+++ PythonProjects/Ejercicios/Untitled-1.py
@@ -1,6 +1,21 @@
-nombre = input("¿Cual es tu nombre?")
-def saludar(nombre):
-    print("Hola",nombre)
+import math
 
-saludar(nombre)
+
+angulo_grados = 45
+n_terminos = 100
+
+x_radianes = math.radians(angulo_grados)
+
+
+seno_aproximado = 0
+
+
+for i in range(n_terminos):
+ 
+    termino = ((-1)**i) * (x_radianes**(2 * i + 1)) / math.factorial(2 * i + 1)
+    
+    
+    seno_aproximado += termino
+
+print(f"El seno de {angulo_grados} grados es aproximadamente: {seno_aproximado}")
 
